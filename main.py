a3237b5bf38c32fbc9bace5798dc9d4c1dbb16a5
Zunaira <76596122+zzunairaa@users.noreply.github.com>
2026-03-30 01:12:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ def summarize(req: SummarizeRequest):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=False)
